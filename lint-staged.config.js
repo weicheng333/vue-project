b71142d10022c31d17a75e8fcf616502da41c25d
liu <1622397316@qq.com>
2025-06-12 21:39:08 +0800

    test

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,7 +1,7 @@
 export default {
   '*.{ts,js,vue,json}': stagedFiles => [
     `eslint ${stagedFiles.join(' ')}`,
-    `vue-tsc --noEmit ${stagedFiles.filter(file => !file.endsWith('json') && !file.endsWith('js')).join(' ')}`,
+    `vue-tsc --noEmit  ${stagedFiles.filter(file => !file.endsWith('json') && !file.endsWith('js')).join(' ')}`,
   ],
   '*.{css,vue,less}': stagedFiles => [`stylelint ${stagedFiles.join(' ')}`],
 }
