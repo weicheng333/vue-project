7aff4a4f554194229a9c8344e01102566698e1f3
liu <1622397316@qq.com>
2025-06-12 21:38:40 +0800

    test

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ const app = createApp(App)
 InitPinia(app)
 InitRouter(app)
 
-console.log(import.meta.env, '---env')
+console.log(import.meta.env, '---env', '222')
 
 app.config.errorHandler = (err) => {
   console.log(err)
